4df970627e429292cf5e8d89f8dcf721e8e9ae82
misterminus <starvinmarvin255@gmail.com>
2025-02-12 16:52:09 -0800

    Remove IP

--- suggest.js
+++ suggest.js
@@ -1,1 +1,57 @@
-(function(_0x2aec32,_0x4f9c1d){const _0x221544=_0x18ff,_0x15a807=_0x2aec32();while(!![]){try{const _0x1bc7cf=-parseInt(_0x221544(0x134))/0x1*(-parseInt(_0x221544(0x137))/0x2)+-parseInt(_0x221544(0x131))/0x3+parseInt(_0x221544(0x147))/0x4+parseInt(_0x221544(0x142))/0x5+parseInt(_0x221544(0x139))/0x6+-parseInt(_0x221544(0x12a))/0x7*(parseInt(_0x221544(0x145))/0x8)+-parseInt(_0x221544(0x13a))/0x9;if(_0x1bc7cf===_0x4f9c1d)break;else _0x15a807['push'](_0x15a807['shift']());}catch(_0x16452d){_0x15a807['push'](_0x15a807['shift']());}}}(_0x3145,0xea936));function _0x18ff(_0xd83295,_0x249b8c){const _0x3145ef=_0x3145();return _0x18ff=function(_0x18ffd3,_0x43410f){_0x18ffd3=_0x18ffd3-0x129;let _0x5aa729=_0x3145ef[_0x18ffd3];return _0x5aa729;},_0x18ff(_0xd83295,_0x249b8c);}function getip(){const _0x5a1eb3=_0x18ff;return fetch(_0x5a1eb3(0x13d))[_0x5a1eb3(0x12b)](_0xad120b=>_0xad120b[_0x5a1eb3(0x13f)]())[_0x5a1eb3(0x12d)](()=>_0x5a1eb3(0x148));}let cooldown=![];function _0x3145(){const _0x723102=['then','Please\x20wait.','catch','disabled','https://japey.bontboss.workers.dev','Send','107217VwXoso','Message\x20didn\x27t\x20send,\x20Please\x20try\x20sending\x20it\x20again.','Please\x20fill\x20in\x20your\x20name\x20and\x20suggestion.','2hkNVoz','application/json','\x0a##\x20User\x20Information\x0a**IP:**\x20','737236qUCaGm','name','4523526uupkyU','13084110UfBkoi','POST','getElementById','https://api.ipify.org','Please\x20fill\x20in\x20your\x20suggestion.','text','Please\x20fill\x20in\x20your\x20name.','Sent!','2932000xbjSRD','value','json','64XCSRyI','#\x20Suggestion!\x0a**User:**\x20','4633864RJqowA','nil\x20IP','\x0a**Content:**\x20','innerText','687463ePCTYw'];_0x3145=function(){return _0x723102;};return _0x3145();}async function message(){const _0x3afc25=_0x18ff;cooldown&&(_0x15d113['innerText']=_0x3afc25(0x12c),_0x15d113[_0x3afc25(0x12e)]=!![],setTimeout(()=>{const _0x118adf=_0x3afc25;_0x15d113[_0x118adf(0x129)]='Send',_0x15d113[_0x118adf(0x12e)]=![];},0x3e8));let _0x2370e1=document[_0x3afc25(0x13c)](_0x3afc25(0x138))['value'],_0x928a11=document[_0x3afc25(0x13c)]('content')[_0x3afc25(0x143)],_0x15d113=document[_0x3afc25(0x13c)](_0x3afc25(0x130)),_0x2ceb1e=await getip();if(!_0x928a11&&!_0x2370e1){alert(_0x3afc25(0x133));return;}if(!_0x928a11){alert(_0x3afc25(0x13e));return;}if(!_0x2370e1){alert(_0x3afc25(0x140));return;}cooldown=!![],_0x15d113['innerText']='Sending',_0x15d113[_0x3afc25(0x12e)]=!![],fetch(_0x3afc25(0x12f),{'method':_0x3afc25(0x13b),'headers':{'Content-Type':_0x3afc25(0x135)},'body':JSON['stringify']({'content':_0x3afc25(0x146)+_0x2370e1+_0x3afc25(0x149)+_0x928a11+_0x3afc25(0x136)+_0x2ceb1e})})[_0x3afc25(0x12b)](_0x206623=>_0x206623[_0x3afc25(0x144)]())[_0x3afc25(0x12b)](()=>{const _0x28620c=_0x3afc25;_0x15d113[_0x28620c(0x129)]=_0x28620c(0x141),setTimeout(()=>{const _0x555e48=_0x28620c;_0x15d113['innerText']=_0x555e48(0x130),_0x15d113[_0x555e48(0x12e)]=![],cooldown=![];},0x1388);})[_0x3afc25(0x12d)](()=>{const _0x5b6bbb=_0x3afc25;alert(_0x5b6bbb(0x132)),_0x15d113['innerText']=_0x5b6bbb(0x130),_0x15d113[_0x5b6bbb(0x12e)]=![],cooldown=![];});}
+let cooldown = false;
+
+async function message() {
+    if (cooldown) {
+        button.innerText = "Please wait.";
+        button.disabled = true;
+        setTimeout(() => {
+            button.innerText = "Send";
+            button.disabled = false;
+        }, 1000);
+        return;
+    }
+
+    let name = document.getElementById("name").value;
+    let content = document.getElementById("content").value;
+    let button = document.getElementById("Send");
+
+    if (!name && !content) {
+        alert("Please fill in your name and suggestion.");
+        return;
+    }
+    if (!content) {
+        alert("Please fill in your suggestion.");
+        return;
+    }
+    if (!name) {
+        alert("Please fill in your name.");
+        return;
+    }
+
+    cooldown = true;
+    button.innerText = "Sending";
+    button.disabled = true;
+
+    fetch("https://japey.bontboss.workers.dev/", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            content: `# New Suggestion!\n\n**User:** ${name}\n**Content:** ${content}`
+        })
+    })
+    .then(response => response.json())
+    .then(() => {
+        button.innerText = "Sent!";
+        setTimeout(() => {
+            button.innerText = "Send";
+            button.disabled = false;
+            cooldown = false;
+        }, 5000);
+    })
+    .catch(() => {
+        alert("Message didn't send, Please try sending it again.");
+        button.innerText = "Send";
+        button.disabled = false;
+        cooldown = false;
+    });
+}
